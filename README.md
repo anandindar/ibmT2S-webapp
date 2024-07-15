642a0d0724dca5749f324095815bb6d4f5c7bab8
Anand Kumar Gupta <anandkumargupta0814@gmail.com>
2024-07-15 11:32:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+# Text-to-Speech Web App
+
+## Overview
+This web app uses IBM Cloud Text-to-Speech API to convert text into speech. It's built with Streamlit for a user-friendly interface.
+
+## Features
+- Input text in a user-friendly interface.
+- Choose voice preferences using the IBM Cloud Text-to-Speech API.
+
+
+## Prerequisites
+Make sure you have the following installed:
+- Python
+- Streamlit
+- IBM Cloud Text-to-Speech API credentials
+
+## Installation
+1. Clone the repository: `git clone https://github.com/CrimsonREwind/text2speech_webapp.git`
+2. Install dependencies: `pip install -r requirements.txt`
+3. Set up your IBM Cloud Text-to-Speech API credentials.
+
+## Screenshot
+![Screenshot from 2024-03-05 07-27-06](https://github.com/CrimsonREwind/text2speech_webapp/assets/106526797/4c97a3a7-f6e4-4740-a35a-7280bac15134)
+
+![Screenshot from 2024-03-05 07-27-30](https://github.com/CrimsonREwind/text2speech_webapp/assets/106526797/edff03e6-4479-462e-aa8c-9a4d9d208f32)
+
+![Screenshot from 2024-03-05 07-30-01](https://github.com/CrimsonREwind/text2speech_webapp/assets/106526797/15a3d73b-a443-4043-ab0c-032665478439)
+
+
+## Usage
+1. Run the Streamlit app: `streamlit run app.py`
+2. Visit `http://localhost:8501` in your browser to access the web app.
+3. set up ibm cloud text to speech api-key and url.
+4. Input text, select voice preferences, and generate speech.
+
+## IBM Cloud Text-to-Speech API Credentials
+Obtain your API key and URL from IBM Cloud.
+
+## Sample
+
+Input: 
+
+```"Hello there! This is a test for text-to-speech functionality. I hope this message finds you well. Technology is amazing, isn't it? It's fascinating how computers can convert written words into spoken ones. Let me know how this sounds on your end, and if there's anything else you'd like to try!"```
+
+Output: 
+
+[Generated audio](https://raw.githubusercontent.com/CrimsonREwind/text2speech_webapp/16603ad9a7b0c12cba52391d617c8d74a55abc1f/sample/audio/generated.wav)
+
+[Try Webapp without downloading](https://huggingface.co/spaces/TRTS/t2sibm_webapp)
+
+## Contributing
+Feel free to contribute to the project! If you find bugs, have feature requests, or want to improve the code, follow these steps:
+
+1. Fork the repository.
+2. Create a new branch: `git checkout -b feature/bugfix-your-feature`.
+3. Commit your changes: `git commit -m 'Add some feature'`.
+4. Push to the branch: `git push origin feature/bugfix-your-feature`.
+5. Submit a pull request.
---
+++ app.py
@@ -0,0 +1,58 @@
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+import streamlit as st
+
+exp=st.sidebar.expander("Settings")
+api=exp.text_input("API-Key")
+url=exp.text_input("Url")
+label="Get API-key"
+link = "https://cloud.ibm.com/resources"
+exp.link_button(label, link, help=None, type="secondary", disabled=False, use_container_width=False)
+
+st.title("Text To Speech")
+
+
+def on_button_click():
+    st.sidebar.success("Generating Audio File")
+    file_name=file_input+'.wav'
+    print(file_name)
+    with open(file_name, 'wb') as audio_file:
+        audio_file.write(
+            text_to_speech.synthesize(
+                text_input,
+                voice=voice_sel,
+                accept='audio/wav',
+                pitch_percentage = pitch,
+                rate_percentage = speed
+
+            ).get_result().content)
+        
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_directory, file_name)
+
+    if os.path.exists(file_path):
+        st.sidebar.success("text to speech generated successfully")
+    else:
+        st.sidebar.error("unsuccessfull")
+    st.audio(file_path)
+
+
+authenticator = IAMAuthenticator(api)
+text_to_speech = TextToSpeechV1(
+    authenticator=authenticator
+)
+
+text_to_speech.set_service_url(url)
+
+text_input=st.text_input("Text to convert into speech")
+file_input=st.text_input("File Name")
+voice_sel=st.selectbox("Select a Voice", ["en-AU_HeidiExpressive", "en-US_LisaExpressive", "en-US_EmmaExpressive", "en-US_AllisonExpressive", "de-DE_BirgitVoice", "en-US_MichaelVoice", "ko-KR_JinV3Voice", "en-AU_JackExpressive", "fr-FR_ReneeVoice", "de-DE_DieterVoice", "en-US_MichaelExpressive", "en-US_AllisonVoice", "en-GB_KateVoice", "de-DE_BirgitV3Voice", "de-DE_DieterV3Voice", "en-US_HenryV3Voice", "es-ES_EnriqueV3Voice", "en-US_LisaVoice", "ja-JP_EmiVoice", "en-US_MichaelV2Voice", "ko-KR_JinV2Voice", "es-ES_EnriqueVoice", "fr-CA_LouiseV3Voice", "es-LA_SofiaVoice", "es-ES_LauraVoice", "nl-NL_MerelV3Voice", "en-GB_KateV3Voice", "en-US_OliviaV3Voice", "pt-BR_IsabelaV3Voice", "en-US_KevinV3Voice", "it-IT_FrancescaVoice", "de-DE_DieterV2Voice", "ja-JP_EmiV3Voice", "en-US_AllisonV3Voice", "de-DE_ErikaV3Voice", "en-US_LisaV2Voice", "fr-FR_NicolasV3Voice", "en-US_MichaelV3Voice", "en-US_EmilyV3Voice", "en-GB_CharlotteV3Voice", "pt-BR_IsabelaVoice", "es-US_SofiaVoice", "de-DE_BirgitV2Voice", "it-IT_FrancescaV3Voice", "it-IT_FrancescaV2Voice", "en-US_AllisonV2Voice", "es-US_SofiaV3Voice", "en-US_LisaV3Voice", "fr-FR_ReneeV3Voice"])
+
+file_name=file_input+'.wav'
+pitch = st.slider('pitch', -100, 100, 0)
+speed = st.slider('speed', -100, 100, 0)
+
+
+if st.button("Generate"):
+    on_button_click()
